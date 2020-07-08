7e62367c87ef4ed9522ec3a5491ea1534e85db43
hj <>
2020-07-08 10:45:44 +0800

    Redirect to redirect uri

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import qs from 'qs';
 
 function App() {
+  const queryString = qs.parse(window.location.search.substr(1));
+
+  const { redirect_uri, state } = queryString;
+
+  const URLParams = {
+    code: 'some_auth_code',
+    state,
+  };
+
+  window.location.href = `${redirect_uri}?${qs.stringify(URLParams)}`;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Mock Singpass Server
       </header>
     </div>
   );
